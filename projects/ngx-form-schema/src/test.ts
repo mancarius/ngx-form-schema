1043d1da92ade893628a81a7168aaec74c770eb7
Mattia Mancarella <mattia.mancarella.dev@gmail.com>
2023-05-03 13:04:15 +0200

    app: set test environment for CI

--- projects/ngx-form-schema/src/test.ts
+++ projects/ngx-form-schema/src/test.ts
@@ -1,27 +1,22 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
-import 'zone.js';
-import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+import { ngMocks } from 'ng-mocks';
 
-declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    <T>(id: string): T;
-    keys(): string[];
-  };
-};
+// Initialize the Angular testing environment.
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  errorOnUnknownElements: true,
+  errorOnUnknownProperties: true
+});
 
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-);
+// auto spy
+ngMocks.autoSpy('jasmine');
 
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().forEach(context);
+import { CommonModule } from '@angular/common'; // eslint-disable-line import/order
+import { ApplicationModule } from '@angular/core'; // eslint-disable-line import/order
+import { BrowserModule } from '@angular/platform-browser'; // eslint-disable-line import/order
+ngMocks.globalKeep(ApplicationModule, true);
+ngMocks.globalKeep(CommonModule, true);
+ngMocks.globalKeep(BrowserModule, true);
