b8c46c0c29555fabc487752632af5cc48c28ab20
Mattia Mancarella <mattia.mancarella.dev@gmail.com>
2023-05-22 18:20:20 +0200

    lib: add form-array-schema to public-api

--- projects/ngx-form-schema/src/public-api.ts
+++ projects/ngx-form-schema/src/public-api.ts
@@ -8,4 +8,5 @@ export * from './lib/directives';
 export * from './lib/services/ngx-form-schema-builder.service';
 export * from './lib/models/form-control-schema';
 export * from './lib/models/form-group-schema';
+export * from './lib/models/form-array-schema';
 export * from './lib/ngx-form-schema.module';
